d9d53018e5f103b324154e30908946589bdf757e
Radhika Senthil Kumar <radhee.senthil@gmail.com>
2022-11-03 11:47:13 -0500

    Initial Commit

---
+++ MyNikeSample/MyNikeSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyNikeSampleApp.swift
+//  MyNikeSample
+//
+//  Created by Radhika Senthil on 11/3/22.
+//
+
+import SwiftUI
+
+@main
+struct MyNikeSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
